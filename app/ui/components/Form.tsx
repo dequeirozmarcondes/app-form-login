7d1e54faf11463df94eee6751b1838f4545bed04
dequeirozmarcondes <dequeirozmarcondes@outlook.com>
2025-03-11 00:42:45 -0400

    retornando a logo

--- app/ui/components/Form.tsx
+++ app/ui/components/Form.tsx
@@ -7,8 +7,7 @@ export function Form() {
     return (
         <form className="bg-slate-700 p-6 rounded-xl shadow-lg flex flex-col gap-4 w-96 h-[30rem]">
             <div className="flex justify-center">
-                {/* <Image src="/logo.png" alt="Logo" width={200} height={200} className="mx-auto" /> */}
-                Teste
+                <Image src="/logo.png" alt="Logo" width={200} height={200} className="mx-auto" />
             </div>
             <InputField label="Nome" type="text" name="name" icon={<User size={20} />} />
             <InputField label="E-mail" type="email" name="email" icon={<Mail size={20} />} />
